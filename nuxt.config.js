d0e9674d1af0921d0fb6ab6c98f995a1ef10bb74
Reuben Jacob Mathew <rjmat97@gmail.com>
2021-05-03 21:16:45 +0530

    removing dist from main branch

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/website/'
-  },
+  // router: {
+  //   base: '/website/'
+  // },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'website',
